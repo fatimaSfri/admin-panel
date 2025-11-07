8392687192dd5cdf90c4f2bc0b1a602cf68efe03
fatima <sfateme262@gmail.com>
2025-11-07 08:53:27 +0330

    change style v-4

--- src/schemas/resetPassword.ts
+++ src/schemas/resetPassword.ts
@@ -2,15 +2,17 @@ import { z } from 'zod';
 
 export const resetPasswordSchema = z
   .object({
-    password: z
-      .string()
-      .min(6, 'Password must be at least 6 characters long.'),
-    confirmPassword: z
-      .string()
-      .min(6, 'Password must be at least 6 characters long.'),
+    password: z.preprocess(
+      (val) => val ?? '',
+      z.string().min(6, 'Password must be at least 6 characters long.')
+    ),
+    confirmPassword: z.preprocess(
+      (val) => val ?? '',
+      z.string().min(6, 'Password must be at least 6 characters long.')
+    ),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message:'Password and repeat password are not the same!',
+    message: 'Password and repeat password are not the same!',
     path: ['confirmPassword'],
   });
 
